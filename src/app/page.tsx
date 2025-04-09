9e674b6902d6f799e0cd4cff7339150205a6f7b1
Matthew Guy <me@mattguy.me>
2025-04-09 14:44:08 -0700

    displaying boards and assets with infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,65 @@
+"use client"
+
+import { Board, fetchBoards } from "@/api/boards";
+import { ClipsListResponse, fetchAssets } from "@/api/clips";
+import { BoardView } from "@/components/BoardView";
+import { useCallback, useEffect, useState } from "react";
+
 export default function Home() {
-  return <main></main>;
+  const [boards, setBoards] = useState<Board[]>([])
+
+  const [isLoadingClips, setIsLoadingClips] = useState(true)
+  const [clipsResponse, setClipsResponse] = useState<ClipsListResponse['data'] & {pagination: Pick<ClipsListResponse['pagination'], 'cursor' | 'hasMore'>}>({
+    total: 0,
+    clips: [],
+    pagination: {
+      cursor: null,
+      hasMore: true,
+    },
+  })
+
+  // initial load
+  useEffect(() => {
+    fetchBoards().then((boards) => setBoards(boards.data))
+    fetchAssets({cursor: null}).then((assets) => {
+      setClipsResponse({...assets.data, pagination: assets.pagination})
+      setIsLoadingClips(false)
+    })
+  }, [])
+
+  // load more clips...
+  const loadMore = useCallback(async () => {
+    if (isLoadingClips || !clipsResponse.pagination.hasMore) return
+    
+    setIsLoadingClips(true)
+
+    try {
+      const response = await fetchAssets({ cursor: clipsResponse.pagination.cursor })
+
+      setClipsResponse(prev => ({
+        total: response.data.total,
+        clips: [...prev.clips, ...response.data.clips],
+        pagination: response.pagination,
+      }))
+    } catch (error) {
+      console.error('Error loading more clips:', error)
+    } finally {
+      setIsLoadingClips(false)
+    }
+  }, [isLoadingClips])
+
+  // ...when scrolling to the bottom of the page
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+        loadMore()
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [loadMore])
+
+  return <main><BoardView boards={boards} clipsResponse={clipsResponse} /></main>;
 }
